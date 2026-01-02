6f230913833233eef338f86de632bd7c3e9b2975
pandajakob <jakobm.dev@gmail.com>
2026-01-02 12:02:45 +0100

    implemented depth functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,23 @@
 import java.io.File;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        File root = new File("./");
-        if (root.exists()) {
-            FileTree fileTree = new FileTree(args, root);
-            fileTree.makeTree();
-        } else {
-            System.out.println("error getting root file");
+
+
+
+        try {
+            File root = new File("./");
+
+            if (root.exists()) {
+                FileTree fileTree = new FileTree(args, root);;
+                fileTree.makeTree();
+            } else {
+                System.out.println("error getting root file");
+            }
+        } catch(RuntimeException e){
+            System.out.println("error: "+e.getMessage());
         }
     }
 
@@ -19,54 +25,98 @@ public class Main {
 
 class FileTree {
     private final File root;
+    private final List<FileNode> fileNodes = new ArrayList<>();
+    private final List<String> args = new ArrayList<>();
+    private final int maxDepth;
 
-    private final List<String> args;
+    private int getPermittedDepth() {
+        if (args.contains("-d")) {
+            // finds the int after -d
+            String intString = args.get(args.indexOf("-d")+1);
+            args.remove("-d");
+            args.remove(intString);
+
+            return Integer.parseInt(intString);
+        } else {
+            return 1000;
+        }
+    }
 
     FileTree(String[] args, File root) {
         this.root = root;
-        this.args = List.of(args);
+        this.args.addAll(List.of(args));
+        this.maxDepth = getPermittedDepth();
     }
 
     public void makeTree() {
-        printChildren(root, 0);
+        getChildren(root, 0);
+
+        for (FileNode fileNode : fileNodes) {
+                fileNode.printNodeString();
+        }
 
     }
 
-    private void printChildren(File file, int depth) {
+    private void getChildren(File file, int depth) {
         File[] children = file.listFiles();
-        if (children == null)
+        if (children == null || depth == maxDepth)
             return;
 
         for (File child : children) {
             if (args.contains(child.getName()))
                 continue;
 
-            print(child, depth);
-            printChildren(child, depth+1);
+            fileNodes.add(new FileNode(child, depth));
+            getChildren(child, depth+1);
         }
+    }
+}
+
+class FileNode {
+    private final int depth;
+    private final File file;
+
+
+    public FileNode(File file, int depth) {
+        this.file = file;
+        this.depth = depth;
+    }
+
+    private File getParentFile() {
+        File parentFile = this.file.getParentFile();
+
+        if (parentFile != null)
+            return parentFile;
+        else
+            throw new NoSuchElementException("No parent element for file or is root" + file.getName());
+    }
+
+    private boolean isLastNode() {
+        if (file.getName().equals(".")) {
+            return true;
+        }
+        return List.of(getParentFile().listFiles()).getLast().equals(file);
     }
 
     /* │ ├ ─ */
-    private void print(File file, int depth) {
+    public void printNodeString() {
         StringBuilder printString = new StringBuilder();
 
-        if (depth==0) {
-            if (isLast(file))
+        if (depth==0)
+            if (isLastNode())
                 printString.append("└── ");
             else
                 printString.append("├── ");
 
-        }
-
-        for (int i = 0; i < depth; i++) {
+        for (int i = 0; i < depth; i++)
             printString.append("  ");
-        }
-        if (depth>0) {
-            if (isLast(file))
+
+        if (depth>0)
+            if (isLastNode())
                 printString.append("└── ");
             else
                 printString.append("├── ");
-        }
+
 
         printString.append(file.getName());
 
@@ -75,8 +125,12 @@ class FileTree {
         }
         System.out.println(printString);
     }
-    private boolean isLast(File file) {
-        return List.of(Objects.requireNonNull(file.getParentFile().listFiles())).getLast().equals(file);
+
+    public File getFile() {
+        return file;
     }
 
+    public int getDepth() {
+        return depth;
+    }
 }
